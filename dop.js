3fe115e8b89018cc322fda4de0c38e00f2c25a1b
aunewprinter <78233892+aunewprinter@users.noreply.github.com>
2021-09-29 01:00:08 +0300

    Построить Город-Сад Fixes #7

--- dop.js
+++ dop.js
@@ -1,1 +1,13 @@
 // TODO Переделать сервер
+// TODO https://github.com/aunewprinter/test/issues/4
+// TODO https://github.com/aunewprinter/test/issues/7 @aunewprinter
+
+function main() {
+    const i = 0
+
+    for (let i = 0; i < array.length; i++) {
+        const element = array[i];
+
+    }
+    return
+}
