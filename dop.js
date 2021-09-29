2a93a9e9f27aa62e548a6078189339eacf9cf426
aunewprinter <78233892+aunewprinter@users.noreply.github.com>
2021-09-29 10:09:28 +0300

    Фиксация #7 проблемы (#13)

--- dop.js
+++ dop.js
@@ -1,6 +1,11 @@
 // TODO Переделать сервер
 // TODO https://github.com/aunewprinter/test/issues/4
 // TODO https://github.com/aunewprinter/test/issues/7 @aunewprinter
+// TODO #7
+// TODO aunewprinter/test#7
+// https://github.com/aunewprinter/test/issues/2
+
+
 
 function main() {
     const i = 0
